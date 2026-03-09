db99238510cb4babb030424cebae144188dd3496
Jaju8756 <jaju8756@colorado.edu>
2026-03-08 19:16:40 -0600

    init scripts for aesd char device added; fulltest passed

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,12 @@
+AESD_CHAR_DRIVER_VERSION = 9986dee501de896e80223a59b212fd93027a9f64
+AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Jaju8756.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_LICENSE = GPL-2.0
+AESD_CHAR_DRIVER_LICENSE_FILES = LICENSE
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
